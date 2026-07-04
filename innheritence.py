df14eecbbc634d1e53999bc381774af3bca73459
kiran umarani <kiranumarni200@gmail.com>
2026-07-04 11:51:49 +0530

    update code

---
+++ innheritence.py
@@ -0,0 +1,11 @@
+class person:
+    def speak(self):
+        print("hello i am person")
+
+class male(person):
+    def  launguage(self):
+        print("my name is kiran\n my gender is male")  
+
+k=male()
+k.speak()
+k.launguage()        
